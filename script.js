2cb1d2098728838b2483a5b96df8267dc0b7e69b
GPS01 <jenny.perugu2000@gmail.com>
2024-10-07 18:14:51 +0000

    <strong>love</strong>

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const modal = document.getElementById("disclaimer-modal");
-    const agreeBtn = document.getElementById("agree-btn");
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.getElementById("disclaimer-modal");
+  const agreeBtn = document.getElementById("agree-btn");
 
-    agreeBtn.addEventListener("click", function() {
-        modal.style.display = "none";
-    });
+  agreeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var hamburger = document.querySelector('.hamburger');
-    var navMenu = document.querySelector('.navbar');
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".navbar");
 
-    hamburger.addEventListener('click', function () {
-        // Toggle class 'open' on navbar
-        navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
-    });
+  hamburger.addEventListener("click", function () {
+    // Check the current display status and toggle it
+    if (navbar.style.display === "block") {
+      navbar.style.display = "none";
+    } else {
+      navbar.style.display = "block";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Glider(document.querySelector(".glider"), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: ".dots",
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
+  });
 });
 
